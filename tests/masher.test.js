9ab07961b4e4158a64971990aa8a3e22a64db273
Greg Allen <me@jga.me>
2011-10-25 22:01:16 -0700

    added two more unit tests

--- tests/masher.test.js
+++ tests/masher.test.js
@@ -1,3 +1,6 @@
+var exec = require('child_process').exec;
+var path = require('path');
+var fs = require('fs');
 var testCase = require('nodeunit').testCase;
 var Masher = require('../').Masher;
 
@@ -33,15 +36,57 @@ module.exports = testCase({
   compressJS: function(t) {
     var masher = new Masher({
       basePath: 'example/public', 
-      compressPath: 'compressed',
       compress: true
     });
     masher.require('header', 'common', ['ui/scripts/external/ender.js', 'ui/scripts/app.js']);
     var output = masher.block('header');
     t.ok(masher._scripts.header.common.compressed);
-    console.log(output);
     t.done();
+  },
+  restartTest: function(t) {
+    var opt = {
+      basePath: 'example/public', 
+      compress: true
+    };
+    var masher1 = new Masher(opt);
+    masher1.require('header', 'common', ['ui/scripts/app.js']);
+    masher1.block('header');
+    var masher2 = new Masher(opt);
+    masher2.require('header', 'common', ['ui/scripts/app.js']);
+    masher2.block('header');
+    t.ok(masher1._scripts.header.common.compressed);
+    t.equal(masher1._scripts.header.common.compressed, masher2._scripts.header.common.compressed);
+    var dir = path.join(path.join(__dirname, "../"), opt.basePath);
+    var out1 = fs.readFileSync(dir+masher1._scripts.header.common.compressed, 'utf8');
+    var out2 = fs.readFileSync(dir+masher2._scripts.header.common.compressed, 'utf8');
+    t.equal(out1, out2);
+    t.done();
+  },
+  fileChange: function(t) {
+    var opt = {
+      basePath: 'example/public', 
+      compress: true
+    };
+    var masher1 = new Masher(opt);
+    masher1.require('header', 'common', ['ui/scripts/app.js']);
+    masher1.block('header');
+
+    var dir = path.join(path.join(__dirname, "../"), opt.basePath);
+    var file = path.join(dir, 'ui/scripts/app.js');
+    exec('echo "var a = 1;" >> '+file, function() {
+
+      var masher2 = new Masher(opt);
+      masher2.require('header', 'common', ['ui/scripts/app.js']);
+      masher2.block('header');
+      t.ok(masher1._scripts.header.common.compressed);
+      t.notEqual(masher1._scripts.header.common.compressed, masher2._scripts.header.common.compressed);
+
+      var out1 = fs.readFileSync(dir+masher1._scripts.header.common.compressed, 'utf8');
+      var out2 = fs.readFileSync(dir+masher2._scripts.header.common.compressed, 'utf8');
+      t.notEqual(out1, out2);
+      t.done();
+    });
+
+
   }
-  //test: on restart, don't compress
-  //test: on file change, re-compress
 });
